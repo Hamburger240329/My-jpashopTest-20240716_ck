3213898e0ee3f396894656c0cf85d8a8dc0a4680
ck <5514cyj@gmail.com>
2024-07-16 16:25:05 +0900

    thymeleaf include 기능 진행중 - 교재 보고 연습해보기

--- src/main/java/com/ham1142/jpashopTest_20240716_ck/conteoller/JpaController.java
+++ src/main/java/com/ham1142/jpashopTest_20240716_ck/conteoller/JpaController.java
@@ -2,12 +2,14 @@ package com.ham1142.jpashopTest_20240716_ck.conteoller;
 
 import com.ham1142.jpashopTest_20240716_ck.entity.Member;
 import com.ham1142.jpashopTest_20240716_ck.repository.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -38,6 +40,49 @@ public class JpaController {
         return "info";
 
     }
+
+
+    @GetMapping(value = "/linkTest") // 회원 정보 출력
+    public String linkTest(Model model) {
+
+         return "linkTest";
+
+    }
+
+    @GetMapping(value = "/loginOk") // 회원 정보 출력
+    public String loginOk(HttpServletRequest request, Model model) {
+
+        model.addAttribute("id", request.getParameter("id"));
+        model.addAttribute("pw", request.getParameter("pw"));
+
+         return "loginOk";
+
+    }
+
+    @GetMapping(value = "/iftest") // 회원 정보 출력
+    public String iftest(HttpServletRequest request, Model model) {
+
+        Optional<Member> memberOptional = memberRepository.findById(1L); // 1번 회원 정보
+
+        Member member = memberOptional.get();
+
+        model.addAttribute("member", member);
+
+         return "iftest";
+
+    }
+
+
+    @GetMapping(value = "/members") // 회원 정보 출력
+    public String members(HttpServletRequest request, Model model) { // 모든 회숸 리스트 출력
+
+    List<Member> memberList = memberRepository.findAll(); // 모든 회원 리스트 가져오기
+
+    model.addAttribute("memberList", memberList);
+
+    return "eachTest";
+    }
+
 
 
 
